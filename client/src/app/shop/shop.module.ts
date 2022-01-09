a204d0a6eba83c18ef414fdcbf031e6dbf12b53a
onyekachic <onyekachic@gmail.com>
2022-01-09 23:23:26 +0100

    End of section 10

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
-import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
-
+import { ShopRoutingModule } from './shop-routing.module';
 
 @NgModule({
-  declarations: [
-    ShopComponent,
-    ProductItemComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule
-  ],
-  exports:[ShopComponent]
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
+  imports: [CommonModule, SharedModule, ShopRoutingModule],
 })
-export class ShopModule { }
+export class ShopModule {}
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/ProductType';
 import { delay, map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -24,14 +25,13 @@ export class ShopService {
       params = params.append('typeId', shopParams.typeId.toString());
     }
 
-    if(shopParams.search) {
-      params = params.append('search',shopParams.search)
+    if (shopParams.search) {
+      params = params.append('search', shopParams.search);
     }
 
     params = params.append('sort', shopParams.sort);
     params = params.append('PageIndex', shopParams.pageNumber.toString());
     params = params.append('PageIndex', shopParams.pageSize.toString());
-
 
     return this.http
       .get<IPagination>(this.baseUrl + 'products', {
@@ -44,6 +44,10 @@ export class ShopService {
           return response.body;
         })
       );
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
